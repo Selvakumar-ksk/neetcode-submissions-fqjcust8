fea8672d6e4f27f320baf87861e20eb8839690a7
Selvakumar k <selvakumarkuppusamysk@gmail.com>
2026-06-29 14:45:32 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+       s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+       String s1="";
+       for(int i=s.length()-1;i>=0;i--){
+        s1+= s.charAt(i);
+       }return s.equals(s1);
+    }
+}
